d980f596d4c76a0079f6cfa533cedf73aac25f9c
quuarkk <163318694+quuarkk@users.noreply.github.com>
2026-03-30 13:14:13 +0300

    Update server.py

--- 26/server.py
+++ 26/server.py
@@ -353,8 +353,6 @@ async def calculate_pure_memory(pair: int, day: int, date_str: str,
     delta_unit = timedelta(hours=1) if day == 0 else timedelta(days=1)
     events_in_window = []
     for dt in check_dates:
-        if dt > target_date:
-            continue
         dt_end = dt + delta_unit
         _l = bisect.bisect_left(_CAL_SORTED_DATES, dt)
         _r = bisect.bisect_left(_CAL_SORTED_DATES, dt_end)
